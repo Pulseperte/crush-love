44914c5ca35df84cd8a5ef4efa7847d0c6a12f11
Pulseperte <92534281+Pulseperte@users.noreply.github.com>
2021-10-14 22:37:35 +0700

    Update CONFIG.js

--- CONFIG.js
+++ CONFIG.js
@@ -1,10 +1,10 @@
 const CONFIGDATA = {
-  titleHeader: "Làm người yêu tớ nhé I Love You",
+  titleHeader: "Làm người yêu em nhé I Luv u pặc pặc",
   descriptionHeader:
-    "Nếu cậu thoát ra và không trả lời thì có nghĩa là cậu đã ĐỒNG Ý rồi đấy ^^",
+    "Nếu anh thoát ra và không trả lời thì có nghĩa là anh đã ĐỒNG Ý rồi đấy ^^",
   buttonYes: "Đồng Ý",
   buttonNo: "Không! Còn lâu nhé.",
-  titleModar: "Cảm ơn em vì đồng ý",
+  titleModar: "Cảm ơn anh vì đồng ý",
   descriptionModar:
-    'Tớ biết là cậu sẽ đồng ý làm người yêu tớ mà ^^ </br> Cám ơn cậu đã đồng ý để cho tớ được che chở và bảo vệ cậu.Đối với tớ, cậu mãi là cô bé :)) </br> "Thích cậu nhiều đến mấy, cậu sẽ hiểu thôi. </br>  Tớ thích cậu nhiều đến mấy, cậu sẽ hiểu thôi "',
+    'Em biết là anh sẽ đồng ý làm người yêu em mà ^^ </br> Đối với em anh mãi là ánh mặt trời tỏa sáng :)) </br> "I really really really really like you </br>  And Ɩ want уou, do уou want me, do уou want me, too?   "',
 };
